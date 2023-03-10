f8076626b65d5f8c37097cf0af228f1d91fbf4ef
zhangshuqi <1721452755@qq.com>
2023-03-10 09:23:11 +0800

    add 显示后门触发器

---
+++ backdoor/pic_show.py
@@ -0,0 +1,96 @@
+import argparse
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+from attack.poisoned_dataset import CIFAR10Poison
+
+
+def imshow(img):
+    img = img / 2 + 0.5
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# dataiter=iter(trainloader)
+# images,labels=dataiter.next()
+#
+# imshow(torchvision.utils.make_grid(images))
+# print(''.join('%5s' % classes[labels[j]] for j in range(4)))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Federated Backdoor')
+
+    # base settings
+    parser.add_argument('--dataset', default='MNIST',
+                        help='Which dataset to use (MNIST or CIFAR10, default: MNIST)')
+    parser.add_argument('--nb_classes', default=10, type=int,
+                        help='number of the classification types')
+    parser.add_argument('--load_local', action='store_true',
+                        help='train model or directly load model (default true, if you add this param, then load '
+                             'trained local model to evaluate the performance)')
+    parser.add_argument('--model_name', default='badnets',
+                        help='[badnets, resnet18]')
+    parser.add_argument('--loss', default='mse',
+                        help='Which loss function to use (mse or cross, default: mse)')
+    parser.add_argument('--optimizer', default='sgd',
+                        help='Which optimizer to use (sgd or adam, default: sgd)')
+    parser.add_argument('--global_epochs', default=100,
+                        help='Number of epochs to train backdoor model, default: 100')
+    parser.add_argument('--batch_size', type=int, default=64,
+                        help='Batch size to split dataset, default: 64')
+    parser.add_argument('--test_batch_size', type=int, default=64,
+                        help='Batch size to split dataset, default: 64')
+    parser.add_argument('--num_workers', type=int, default=2,
+                        help='')
+    parser.add_argument('--data_path', default='./data/',
+                        help='Place to load dataset (default: ./dataset/)')
+
+    parser.add_argument('--lr', type=float, default=0.01,
+                        help='Learning rate of the model, default: 0.001')
+    parser.add_argument('--lambda_', type=float, default=0.01,
+                        help='')
+    parser.add_argument('--momentum', type=float, default=0.0001,
+                        help='')
+
+    # federated settings
+    parser.add_argument('--total_num', type=int, default=4)
+    parser.add_argument('--k_workers', type=int, default=3,
+                        help='clients num selected for each epoch')
+    parser.add_argument('--adversary_num', type=int, default=1)
+    parser.add_argument('--local_epoch', type=int, default=2)
+
+    # poison settings
+    parser.add_argument('--poisoning_rate', type=float, default=0.1,
+                        help='poisoning portion for local client (float, range from 0 to 1, default: 0.1)')
+    parser.add_argument('--trigger_label', type=int, default=0,
+                        help='The NO. of trigger label (int, range from 0 to 10, default: 0)')
+    parser.add_argument('--trigger_path', default="./triggers/trigger_10.png",
+                        help='Trigger Path (default: ./triggers/trigger_white.png)')
+    parser.add_argument('--trigger_size', type=int, default=5,
+                        help='Trigger Size (int, default: 5)')
+    args = parser.parse_args()
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    # transform = transforms.Compose([transforms.ToTensor(),
+    #                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    testset = CIFAR10Poison(args, args.data_path, train=False, download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    print('ok')
+
+    dataiter = iter(testloader)
+    images, labels = dataiter.next()
+
+    print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+    imshow(torchvision.utils.make_grid(images))
