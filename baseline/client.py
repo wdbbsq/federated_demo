bd2bb46a15e26f16e9a6bc6fd7b9ee95ffc9a57c
zhangshuqi <1721452755@qq.com>
2023-03-16 22:44:40 +0800

    fix baseline code

--- baseline/client.py
+++ baseline/client.py
@@ -1,26 +1,27 @@
 import torch
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
 from tqdm import tqdm
 
-from utils import init_model 
-from torch.utils.data import DataLoader
+from utils import init_model
 
 
 class Client:
 
-    def __init__(self, args, train_dataset, id=-1):
+    def __init__(self, args, train_dataset, client_id=-1):
         self.args = args
-        self.client_id = id
-        self.local_model = init_model(self.args.model_name)
-        self.train_dataset = train_dataset
+        self.local_epochs = args.local_epochs
+        self.local_model = init_model(args.model_name)
+        self.client_id = client_id
+        self.device = args.device
 
-        all_range = list(range(len(self.train_dataset)))
-        data_len = int(len(self.train_dataset) / self.args.total_workers)
-        train_indices = all_range[id * data_len: (id + 1) * data_len]
-
-        self.train_loader = DataLoader(self.train_dataset,
+        all_range = list(range(len(train_dataset)))
+        data_len = int(len(train_dataset) / args.total_workers)
+        train_indices = all_range[client_id * data_len: (client_id + 1) * data_len]
+        self.train_loader = DataLoader(train_dataset,
                                        batch_size=args.batch_size,
-                                       sampler=torch.utils.data.sampler.SubsetRandomSampler(
-                                           train_indices)
+                                       num_workers=args.num_workers,
+                                       sampler=SubsetRandomSampler(train_indices)
                                        )
 
     def local_train(self, global_model, global_epoch):
@@ -29,25 +30,20 @@ class Client:
         optimizer = torch.optim.SGD(self.local_model.parameters(),
                                     lr=self.args.lr,
                                     momentum=self.args.momentum)
-        training_loss = 0
         self.local_model.train()
-        for e in range(self.args.local_epochs):
-            for batch_id, batch in enumerate(tqdm(self.train_loader)):
-                data, target = batch
-                if torch.cuda.is_available():
-                    data = data.cuda()
-                    target = target.cuda()
+
+        for epoch in range(self.local_epochs):
+            for batch_id, (batch_x, batch_y) in enumerate(tqdm(self.train_loader)):
+                batch_x = batch_x.to(self.device, non_blocking=True)
+                batch_y = batch_y.to(self.device, non_blocking=True)
                 optimizer.zero_grad()
-                output = self.local_model(data)
-                loss = torch.nn.functional.cross_entropy(output, target)
-                # 计算梯度
+                output = self.local_model(batch_x)
+                loss = torch.nn.functional.cross_entropy(output, batch_y)
                 loss.backward()
-                # 自更新
                 optimizer.step()
-                training_loss += loss
         local_update = dict()
         for name, data in self.local_model.state_dict().items():
             local_update[name] = (data - global_model.state_dict()[name])
-        training_loss /= len(self.train_loader)
-        print(f'# Epoch: {global_epoch} Client {self.client_id} loss: {training_loss} \n')
+
+        print(f'# Epoch: {global_epoch} Client {self.client_id}  loss: {loss.item()}\n')
         return local_update
