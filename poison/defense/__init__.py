581530e2f98a71ebff561f70a09fd8ae0411ea73
zhangshuqi <1721452755@qq.com>
2023-03-26 16:20:06 +0800

    fix graph-building doesn't work

--- poison/defense/__init__.py
+++ poison/defense/__init__.py
@@ -4,7 +4,6 @@ from networkx.algorithms.clique import find_cliques
 from utils.gradient import calc_dist
 import matplotlib.pyplot as plt
 
-
 THRESHOLD = 6
 STEP = 0.2
 LAYER_NAME = 'fc2.0.weight'
@@ -12,18 +11,15 @@ LAYER_NAME = 'fc2.0.weight'
 
 def get_clean_updates(model_updates):
     n = len(model_updates)
-    e = None
-    while e is not None:
-        graph = nx.Graph()
-        for i, j in list(combinations(model_updates, 2)):
-            graph.add_node(i[id])
-            graph.add_node(j[id])
-            dist = calc_dist(i['local_update'], j['local_update'], LAYER_NAME)
-            print(dist)
-            if dist < THRESHOLD:
-                graph.add_edge(i['id'], j['id'])
-        nx.draw(graph)
-        plt.show()
-        e = find_cliques(graph)
-
-    return e
+    graph = nx.Graph()
+    for i, j in list(combinations(model_updates, 2)):
+        graph.add_node(i['id'])
+        graph.add_node(j['id'])
+        dist = calc_dist(i['local_update'], j['local_update'], LAYER_NAME)
+        print(dist)
+        if dist < THRESHOLD:
+            graph.add_edge(i['id'], j['id'])
+    nx.draw(graph)
+    plt.show()
+    it = find_cliques(graph)
+    return 1
